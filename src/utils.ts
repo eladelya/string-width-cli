3c12b1d7ac07a34ddce9e7913818c05fbae79580
eladelya <155739205+eladelya@users.noreply.github.com>
2026-08-24 23:46:28 +0300

    Update utils.ts

--- src/utils.ts
+++ src/utils.ts
@@ -14,7 +14,6 @@ export function stripAnsi(str: string): string {
 }
 
 export function isControlCharacter(codePoint: number): boolean {
-    // כל תווי הבקרה למעט טאב (0x0009) שנטופל בנפרד
     if (codePoint >= 0x0000 && codePoint <= 0x001F && codePoint !== 0x0009) return true;
     if (codePoint >= 0x007F && codePoint <= 0x009F) return true;
     return false;
